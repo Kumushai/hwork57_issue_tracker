ee9b56f3c2512177f34661b7601799c769fc01f8
kumushai_urmanbetova <urmanbetovak@gmail.com>
2023-07-11 15:17:00 +0600

    Добавила стили тексту ошибки при валидации.

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.forms import widgets
+from django.utils.html import format_html
 
 from webapp.models import Todo
 
@@ -17,11 +18,19 @@ class TodoForm(forms.ModelForm):
     def clean_content(self):
         content = self.cleaned_data['content']
         if len(content) < 4:
-            raise ValidationError('Content is too short!')
+            error_message = format_html('<span style="color: red;">Content is too short!</span>')
+            raise ValidationError(error_message)
         return content
 
     def clean_details(self):
         details = self.cleaned_data['details']
         if 'impossible' in details:
-            raise ValidationError('Люди ракеты в космос запускают, а ты код исправить не можешь?!')
+            error_message = format_html(
+                '<span style="color: red;">Люди ракеты в космос запускают, '
+                'а ты код исправить не можешь?! Убери слово "impossible"</span>')
+            raise ValidationError(error_message)
         return details
+
+
+
+
