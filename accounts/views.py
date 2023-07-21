0fad19f97fff0cb80d715ecd0a063da7bb37b030
kumushai_urmanbetova <urmanbetovak@gmail.com>
2023-07-21 15:23:32 +0600

    Коммит перед удалением: формы, написанной в login.html (далее будем использовать из partial), класс для входа в accounts/views.py для передачи ошибки.

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,11 @@
-from django.contrib.auth import login, authenticate, logout
-from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
 
 
-# def login_view(request):
-#     context = {}
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return redirect('webapp:index')
-#         else:
-#             context['has_error'] = True
-#     return render(request, 'login.html', context=context)
-#
-#
-# def logout_view(request):
-#     logout(request)
-#     return redirect('webapp:index')
+class MyLoginView(LoginView):
+    template_name = 'login.html'
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        response.context_data['error_message'] = 'Неверное имя пользователя или пароль.'
+        return response
+
